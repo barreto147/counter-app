6f620304c7e1a8a955eda61bde066433c5119ac3
Carlos Barreto <carlos.barreto@ariadnacg.com>
2022-02-20 01:41:33 -0400

    expect - to be

--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,7 +1,12 @@
-test("Debe ser true", () => {
-  const isActive = true;
+describe("Pruebas de demo.test.js", () => {
+  test("Deben ser iguales los strings", () => {
+    //1. Inicializacion
+    const mensaje = "Hola mundo";
 
-  if (isActive) {
-    throw new Error("No esta activo");
-  }
+    //2. Estimulo
+    const mensaje2 = `Hola mundo`;
+
+    //3. Observar el comprotamiento
+    expect(mensaje).toBe(mensaje2);
+  });
 });
